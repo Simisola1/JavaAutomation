325f4de72f9e6e74426b31239d39136b142f9d3c
Simisola1 <simisolaadanri@gmail.com>
2025-01-22 21:35:37 +0000

    Initial commit

---
+++ src/Strings_Concatenation.java
@@ -0,0 +1,27 @@
+public class Strings_Concatenation {
+    public static void main(String[] args){
+        //string and string concat
+        String bird = "hummingbird";
+        String adjective = " is beautiful";
+        String newString = bird + adjective;
+
+        //System.out.println(bird + adjective);
+        //System.out.println(newString);
+
+        // string and primitive concat
+        String movieName ="Matrix: ";
+        int parts = 4;
+        boolean isReleased =false;
+        //System.out.println(""+ parts + isReleased);
+
+        //.concat() method
+
+
+        //System.out.println(movieName.concat(parts +""));
+
+
+
+
+
+    }
+}
